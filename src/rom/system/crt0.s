fe748d6d552081b103ede982aad1bb02981670e7
m <m@bitsnbites.eu>
2020-01-02 10:05:42 +0100

    ROM: Run the selftests

--- src/rom/system/crt0.s
+++ src/rom/system/crt0.s
@@ -108,6 +108,24 @@ bss_cleared:
 
 
     ; ------------------------------------------------------------------------
+    ; Run the selftest.
+    ; ------------------------------------------------------------------------
+
+    ldea    s1, pc, #selftest_text@pc
+    bl      vcon_print
+    ldea    s1, pc, #selftest_callback@pc
+    bl      selftest_run
+
+    ; s1 contains the pass/fail status (pass = all bits set).
+    ldea    s2, pc, #selftest_pass_text@pc
+    bs      s1, 1f
+    ldea    s2, pc, #selftest_fail_text@pc
+1:
+    mov     s1, s2
+    bl      vcon_print
+
+
+    ; ------------------------------------------------------------------------
     ; Initialize the memory allocator.
     ; ------------------------------------------------------------------------
 
@@ -277,6 +295,19 @@ print_mem_info:
     ret
 
 
+    ; ------------------------------------------------------------------------
+    ; Callback routine for the selftest.
+    ; s1 = pass/fail (-1/0)
+    ; s2 = test no.
+    ; ------------------------------------------------------------------------
+
+selftest_callback:
+    ldi     s3, #33
+    and     s4, s1, #9
+    add     s1, s3, s4      ; s1 = "*" for pass, "!" for fail
+    b       vcon_putc
+
+
     .section .rodata
 
     .p2align 2
@@ -304,4 +335,11 @@ mem_info_text_2:
     .asciz  ", "
 mem_info_text_3:
     .asciz  " bytes\n"
+
+selftest_text:
+    .asciz  "\nSelftest: "
+selftest_pass_text:
+    .asciz  " PASS\n\n"
+selftest_fail_text:
+    .asciz  " FAIL\n\n"
 
