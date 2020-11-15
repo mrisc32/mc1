049c3350ca3df5fad5ec5f9f83c89d6487442254
m <m@bitsnbites.eu>
2020-11-15 18:25:05 +0100

    sdram: Make the write buffer BURST_LENGTH independent
    We also align the sdram_dq and sdram_dqm output signals to the rising
    edge of clk.

--- src/rtl/sdram.vhd
+++ src/rtl/sdram.vhd
@@ -494,21 +494,27 @@ begin
   end process;
 
   -- write the next sub-word from the write buffer
-  process (data_reg, sel_n_reg, state, wait_counter)
+  process (reset, clk)
+    variable v_burst_cnt : natural range 0 to BURST_LENGTH := BURST_LENGTH;
   begin
-    -- TODO(m): Add support for other DATA_WIDTH/BURST_LENGTH/etc settings.
-    if state = WRITE and wait_counter = 0 then
-      sdram_dq <= data_reg(SDRAM_DATA_WIDTH-1 downto 0);
-      sdram_dqm(1) <= sel_n_reg(1);
-      sdram_dqm(0) <= sel_n_reg(0);
-    elsif state = WRITE and wait_counter = 1 then
-      sdram_dq <= data_reg(SDRAM_DATA_WIDTH*2-1 downto SDRAM_DATA_WIDTH);
-      sdram_dqm(1) <= sel_n_reg(3);
-      sdram_dqm(0) <= sel_n_reg(2);
-    else
+    if reset = '1' then
       sdram_dq <= (others => 'Z');
-      sdram_dqm(1) <= '0';
-      sdram_dqm(0) <= '0';
+      sdram_dqm <= (others => '0');
+    elsif rising_edge(clk) then
+      if next_state = WRITE and next_state /= state then
+        -- Start a new write burst.
+        v_burst_cnt := 0;
+      elsif v_burst_cnt < BURST_LENGTH then
+        v_burst_cnt := v_burst_cnt + 1;
+      end if;
+
+      if v_burst_cnt < BURST_LENGTH then
+        sdram_dq <= data_reg(SDRAM_DATA_WIDTH*(v_burst_cnt+1)-1 downto SDRAM_DATA_WIDTH*v_burst_cnt);
+        sdram_dqm <= sel_n_reg((SDRAM_DATA_WIDTH/8)*(v_burst_cnt+1)-1 downto (SDRAM_DATA_WIDTH/8)*v_burst_cnt);
+      else
+        sdram_dq <= (others => 'Z');
+        sdram_dqm <= (others => '0');
+      end if;
     end if;
   end process;
 end architecture arch;
