5265b468f1558f968d2a5260906119d9703759c2
m <m@bitsnbites.eu>
2022-02-01 20:48:00 +0100

    rom: Add a colorful "mosaic" background

--- src/rom/main.cpp
+++ src/rom/main.cpp
@@ -19,156 +19,58 @@
 //--------------------------------------------------------------------------------------------------
 
 #include "elf32.hpp"
+#include "mosaic.hpp"
+
+#ifdef ENABLE_CONSOLE
+#include "console.hpp"
+#endif
 
 #include <mc1/leds.h>
 #include <mc1/mfat_mc1.h>
-#include <mc1/mmio.h>
 #include <mc1/sdcard.h>
-
-#ifdef ENABLE_DIAGNOSTICS
-#include <mc1/vconsole.h>
-#include <mc1/vcp.h>
-#endif
-
-#ifdef ENABLE_SELFTEST
-#include <selftest.h>
-#endif
 
 #include <cstdint>
 
 // Defined by the linker script.
-extern char __rom_size;
-extern char __bss_start;
-extern char __bss_size;
 extern char __vram_free_start;
 
-#ifndef ENABLE_DIAGNOSTICS
-// Turn console functions into dummy functions.
-#define vcon_print(msg) ((void)msg)
-#endif
-
 namespace {
+// States for the boot state machine.
+enum class boot_state_t {
+  INITIALIZE,
+  RUN_DIAGNOSTICS,
+  WAIT_FOR_SDCARD,
+  MOUNT_FAT,
+  LOAD_MC1BOOT,
+};
+
+// Frame sync class.
+class frame_sync_t {
+public:
+  frame_sync_t() : m_t(0) {
+    m_last_frame_no = MMIO(VIDFRAMENO);
+  }
+
+  void wait_for_next_frame() {
+    uint32_t frame_no;
+    do {
+      frame_no = MMIO(VIDFRAMENO);
+    } while (frame_no == m_last_frame_no);
+    m_t += frame_no - m_last_frame_no;
+    m_last_frame_no = frame_no;
+  }
+
+  uint32_t t() const {
+    return m_t;
+  }
+
+private:
+  uint32_t m_t;
+  uint32_t m_last_frame_no;
+};
+
 // Boot function type.
 using boot_fun_t = void();
-
-// SD card context.
-sdctx_t s_sdctx;
-
-#ifdef ENABLE_DIAGNOSTICS
-void* s_vcon_mem;
-
-inline uint32_t linker_constant(const char* ptr) {
-  return static_cast<uint32_t>(reinterpret_cast<uintptr_t>(ptr));
-}
-
-template <int N>
-constexpr int digit_scalei() {
-  int scale = 1;
-  for (int i = 0; i < N; ++i) {
-    scale *= 10;
-  }
-  return scale;
-}
-
-template <int N>
-constexpr float digit_scalef() {
-  float scale = 1.0F;
-  for (int i = 0; i < N; ++i) {
-    scale *= 10.0F;
-  }
-  return scale;
-}
-
-template <int N>
-void vcon_print_float(const float x) {
-  auto xi = static_cast<int>(x * digit_scalef<N>());
-  constexpr auto iscale = digit_scalei<N>();
-  vcon_print_dec(xi / iscale);
-  if (N > 0) {
-    auto frac = xi % iscale;
-    char buf[N + 2];
-    buf[0] = '.';
-    buf[N + 1] = 0;
-    for (int i = N; i >= 1; --i) {
-      buf[i] = '0' + (frac % 10);
-      frac /= 10;
-    }
-    vcon_print(buf);
-  }
-}
-
-void print_size(uint32_t size) {
-  static const char* SIZE_SUFFIX[] = {" bytes", " KB", " MB", " GB"};
-  int size_div = 0;
-  while (size >= 1024u && (size & 1023u) == 0u) {
-    size = size >> 10;
-    ++size_div;
-  }
-  vcon_print_dec(static_cast<int>(size));
-  vcon_print(SIZE_SUFFIX[size_div]);
-}
-
-void print_addr_and_size(const char* str, const uint32_t addr, const uint32_t size) {
-  vcon_print(str);
-  vcon_print("0x");
-  vcon_print_hex(addr);
-  vcon_print(", ");
-  print_size(size);
-  vcon_print("\n");
-}
-
-void init_console() {
-  if (s_vcon_mem != NULL) {
-    return;
-  }
-
-  // Use the first free VRAM for the video console framebuffer.
-  // Note: We just assume that it will fit in VRAM.
-  s_vcon_mem = reinterpret_cast<void*>(&__vram_free_start);
-
-  // Show the console.
-  vcon_init(s_vcon_mem);
-  vcon_show(LAYER_1);
-}
-
-void deinit_console() {
-  if (s_vcon_mem != NULL) {
-    vcp_set_prg(LAYER_1, NULL);
-    s_vcon_mem = NULL;
-  }
-}
-
-#ifdef ENABLE_SELFTEST
-void selftest_callback(int pass, int /* test_no */) {
-  vcon_print(pass ? "*" : "!");
-}
-#endif
-
-void run_diagnostics() {
-  vcon_print("\n                      **** MC1 - The MRISC32 computer ****\n\n");
-
-  // Print some memory information etc.
-  print_addr_and_size("ROM:      ", ROM_START, linker_constant(&__rom_size));
-  print_addr_and_size("VRAM:     ", VRAM_START, MMIO(VRAMSIZE));
-  print_addr_and_size("XRAM:     ", XRAM_START, MMIO(XRAMSIZE));
-  print_addr_and_size("\nbss:      ", linker_constant(&__bss_start), linker_constant(&__bss_size));
-
-  // Print CPU info.
-  vcon_print("\n\nCPU Freq: ");
-  vcon_print_float<2>(static_cast<float>(MMIO(CPUCLK)) * (1.0F / 1000000.0F));
-  vcon_print(" MHz\n\n");
-
-#ifdef ENABLE_SELFTEST
-  // Run the selftest.
-  vcon_print("Selftest: ");
-  if (selftest_run(selftest_callback)) {
-    vcon_print(" PASS\n\n");
-  } else {
-    vcon_print(" FAIL\n\n");
-  }
-#endif
-}
-#endif  // ENABLE_DIAGNOSTICS
 
 int read_block_fun(char* ptr, unsigned block_no, void* custom) {
   auto* ctx = reinterpret_cast<sdctx_t*>(custom);
@@ -181,57 +83,122 @@ int write_block_fun(const char*, unsigned, void*) {
   return -1;
 }
 
+#ifdef ENABLE_CONSOLE
 void sdcard_log_fun(const char* msg) {
-  vcon_print(msg);
+  console_t::print(msg);
 }
+#else
+#define sdcard_log_fun nullptr
+#endif
+
 }  // namespace
 
 extern "C" int main(int, char**) {
   sevseg_print("OLLEH ");  // Print a friendly "HELLO".
 
-#ifdef ENABLE_DIAGNOSTICS
-  bool diags_have_been_run = false;
+#ifdef ENABLE_CONSOLE
+  console_t console;
 #endif
+  mosaic_t mosaic;
+  sdctx_t sdctx;
+  frame_sync_t frame_sync;
+
+  auto state = boot_state_t::INITIALIZE;
   while (true) {
-#ifdef ENABLE_DIAGNOSTICS
-    init_console();
-    if (!diags_have_been_run) {
-      run_diagnostics();
+    // Update splash screen.
+    if (state != boot_state_t::INITIALIZE) {
+      frame_sync.wait_for_next_frame();
+      mosaic.update(frame_sync.t());
     }
+
+    // Boot state machine.
+    switch (state) {
+      //--------------------------------------------------------------------------------------------
+      // INITIALIZE
+      //--------------------------------------------------------------------------------------------
+      default:
+      case boot_state_t::INITIALIZE: {
+        auto* mem = reinterpret_cast<void*>(&__vram_free_start);
+        mem = mosaic.init(mem);
+#ifdef ENABLE_CONSOLE
+        console.init(mem);
 #endif
+        state = boot_state_t::RUN_DIAGNOSTICS;
+      } break;
 
-    vcon_print("Insert bootable SD-card... ");
-    while (true) {
-      if (sdcard_init(&s_sdctx, sdcard_log_fun)) {
-        vcon_print("OK!\n");
-        sevseg_print("DRACDS");
-        break;
-      }
-    }
-
-    vcon_print("Mounting FAT filesystem... ");
-    while (true) {
-      if (mfat_mount(&read_block_fun, &write_block_fun, &s_sdctx) == 0) {
-        vcon_print("OK!\n");
-        sevseg_print("TAF   ");
-        break;
-      }
-    }
-
-#ifdef ENABLE_DIAGNOSTICS
-    // Deinitialize the console (free up memory for the boot executable).
-    deinit_console();
+      //--------------------------------------------------------------------------------------------
+      // RUN_DIAGNOSTICS
+      //--------------------------------------------------------------------------------------------
+      case boot_state_t::RUN_DIAGNOSTICS: {
+#ifdef ENABLE_CONSOLE
+        if (!console.diags_have_been_run()) {
+          console.run_diagnostics();
+        }
+        console_t::print("Insert bootable SD-card... ");
 #endif
+        state = boot_state_t::WAIT_FOR_SDCARD;
+      } break;
 
-    uint32_t entry_address;
-    if (elf32::load("MC1BOOT.EXE", entry_address)) {
-      sevseg_print("TOOB  ");
+      //--------------------------------------------------------------------------------------------
+      // WAIT_FOR_SDCARD
+      //--------------------------------------------------------------------------------------------
+      case boot_state_t::WAIT_FOR_SDCARD: {
+        if (sdcard_init(&sdctx, sdcard_log_fun)) {
+#ifdef ENABLE_CONSOLE
+          console_t::print("OK!\nMounting FAT filesystem... ");
+#endif
+          sevseg_print("DRACDS");
+          state = boot_state_t::MOUNT_FAT;
+        }
+      } break;
 
-      // Call the boot function.
-      auto* boot_fun = reinterpret_cast<boot_fun_t*>(entry_address);
-      boot_fun();
-    } else {
-      sevseg_print("LOL     ");
+      //--------------------------------------------------------------------------------------------
+      // MOUNT_FAT
+      //--------------------------------------------------------------------------------------------
+      case boot_state_t::MOUNT_FAT: {
+        if (mfat_mount(&read_block_fun, &write_block_fun, &sdctx) == 0) {
+#ifdef ENABLE_CONSOLE
+          console_t::print("OK!\n");
+#endif
+          sevseg_print("TAF   ");
+          state = boot_state_t::LOAD_MC1BOOT;
+        } else {
+          // Retry the SD card step until we find a valid SD card.
+          state = boot_state_t::WAIT_FOR_SDCARD;
+        }
+      } break;
+
+      //--------------------------------------------------------------------------------------------
+      // LOAD_MC1BOOT
+      //--------------------------------------------------------------------------------------------
+      case boot_state_t::LOAD_MC1BOOT: {
+        // TODO(m): Here we should just stat the exe file and fail back to WAIT_FOR_SDCARD if it
+        // does not exist instead of falling back all the way to INITIALIZE.
+
+        // Deinitialize video (blank it while loading the boot executable).
+#ifdef ENABLE_CONSOLE
+        console.deinit();
+#endif
+        mosaic.deinit();
+
+        // Try to load the boot executable.
+        uint32_t entry_address = 0;
+        if (elf32::load("MC1BOOT.EXE", entry_address)) {
+          sevseg_print("TOOB  ");
+
+          // Call the boot function.
+          auto* boot_fun = reinterpret_cast<boot_fun_t*>(entry_address);
+          boot_fun();
+
+          // TODO(m): If the boot program returns (it shouldn't!), we probably
+          // need to do a soft reset in order to get back a valid stack etc.
+        } else {
+          sevseg_print("LOL     ");
+        }
+
+        // Since we deinitialized before, we need to go to initialize again.
+        state = boot_state_t::INITIALIZE;
+      } break;
     }
   }
 
---
+++ src/rom/mosaic.hpp
@@ -0,0 +1,135 @@
+// -*- mode: c; tab-width: 2; indent-tabs-mode: nil; -*-
+//--------------------------------------------------------------------------------------------------
+// Copyright (c) 2022 Marcus Geelnard
+//
+// This software is provided 'as-is', without any express or implied warranty. In no event will the
+// authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose, including commercial
+// applications, and to alter it and redistribute it freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not claim that you wrote
+//     the original software. If you use this software in a product, an acknowledgment in the
+//     product documentation would be appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+//     being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
+//--------------------------------------------------------------------------------------------------
+
+#include <mc1/mmio.h>
+#include <mc1/vcp.h>
+
+#include <mr32intrin.h>
+
+#include <cstdint>
+
+// Note: Using an anonymous namespace saves a few bytes of code size.
+namespace {
+
+// Mosaic background class.
+class mosaic_t {
+public:
+  void* init(void* mem) {
+    // "Allocate" memory.
+    auto* pixels = reinterpret_cast<uint32_t*>(mem);
+    auto* vcp_start = &pixels[MOSAIC_W * MOSAIC_H];
+
+    // Get the HW resolution.
+    const auto native_width = MMIO(VIDWIDTH);
+    const auto native_height = MMIO(VIDHEIGHT);
+
+    // VCP prologue.
+    auto* vcp = vcp_start;
+    *vcp++ = vcp_emit_setreg(VCR_XINCR, (0x010000 * MOSAIC_W) / native_width);
+    *vcp++ = vcp_emit_setreg(VCR_CMODE, CMODE_RGBA8888);
+
+    // Address pointers.
+    uint32_t vcp_pixels_addr = to_vcp_addr(reinterpret_cast<uintptr_t>(pixels));
+    *vcp++ = vcp_emit_waity(0);
+    *vcp++ = vcp_emit_setreg(VCR_HSTOP, native_width);
+    *vcp++ = vcp_emit_setreg(VCR_ADDR, vcp_pixels_addr);
+    for (int k = 1; k < MOSAIC_H; ++k) {
+      auto y = (static_cast<uint32_t>(k) * native_height) / static_cast<uint32_t>(MOSAIC_H);
+      vcp_pixels_addr += MOSAIC_W;
+      *vcp++ = vcp_emit_waity(y);
+      *vcp++ = vcp_emit_setreg(VCR_ADDR, vcp_pixels_addr);
+    }
+
+    // VCP epilogue: Wait forever.
+    *vcp++ = vcp_emit_waity(32767);
+
+    // Set up the VCP address.
+    vcp_set_prg(LAYER_1, vcp_start);
+
+    m_pixels = pixels;
+
+    return reinterpret_cast<void*>(vcp);
+  }
+
+  void deinit() {
+    vcp_set_prg(LAYER_1, nullptr);
+  }
+
+  void update(const uint32_t t) {
+    uint32_t t2 = t >> 2;
+
+    // Define the four corner colors.
+    abgr32_t p11 = make_color(t2);
+    abgr32_t p12 = make_color(t2 + 3433U);
+    abgr32_t p21 = make_color(1150U - t2);
+    abgr32_t p22 = make_color(t2 + 13150U);
+
+    // Interpolate all the "pixels" (tiles) in the mosaic.
+    uint32_t* pixels = m_pixels;
+    for (int y = 0; y < MOSAIC_H; ++y) {
+      uint32_t wy = (y << 8) / MOSAIC_H;
+      abgr32_t p1 = lerp(p11, p21, wy);
+      abgr32_t p2 = lerp(p12, p22, wy);
+      for (int x = 0; x < MOSAIC_W; ++x) {
+        uint32_t wx = (x << 8) / MOSAIC_W;
+        *pixels++ = lerp(p1, p2, wx);
+      }
+    }
+  }
+
+private:
+  // Color type.
+  using abgr32_t = uint32_t;
+
+  static const int MOSAIC_W = 16;
+  static const int MOSAIC_H = (MOSAIC_W * 9) / 16;
+
+  static abgr32_t lerp(const abgr32_t c1, const abgr32_t c2, uint32_t w2) {
+    uint32_t w1 = 255U - w2;
+#ifdef __MRISC32_PACKED_OPS__
+    uint8x4_t w1p = _mr32_shuf(w1, _MR32_SHUFCTL(0, 0, 0, 0, 0));  // Splat
+    uint8x4_t w2p = _mr32_shuf(w2, _MR32_SHUFCTL(0, 0, 0, 0, 0));
+    return _mr32_mulhiu_b(w1p, c1) + _mr32_mulhiu_b(w2p, c2);
+#else
+    uint32_t br = ((w1 * (c1 & 0xff00ffU) + w2 * (c2 & 0xff00ffU)) >> 8) & 0xff00ffU;
+    uint32_t g = ((w1 * (c1 & 0x00ff00U) + w2 * (c2 & 0x00ff00U)) >> 8) & 0x00ff00U;
+    return br | g;
+#endif
+  }
+
+  static uint32_t tri_wave(uint32_t t) {
+    uint32_t t_mod = t & 511U;
+    return t_mod <= 255U ? t_mod : 511U - t_mod;
+  }
+
+  static abgr32_t make_color(uint32_t t) {
+    uint32_t tr = t;
+    uint32_t tg = t + 90U;
+    uint32_t tb = 160U - t;
+    uint32_t r = tri_wave(tr);
+    uint32_t g = tri_wave(tg);
+    uint32_t b = tri_wave(tb);
+    return r | (g << 8) | (b << 16);
+  }
+
+  uint32_t* m_pixels;
+};
+
+}  // namespace
