a6fa53bae8cfcda40ed102bfdf8d77b0f6d9f664
m <m@bitsnbites.eu>
2022-12-15 22:02:31 +0100

    rtl: Update xram_sdram to the new sdram_controller

--- src/rtl/xram_sdram.vhd
+++ src/rtl/xram_sdram.vhd
@@ -34,19 +34,19 @@ entity xram_sdram is
     CPU_CLK_HZ : integer;
 
     -- See sdram.vhd for the details of these generics.
-    SDRAM_ADDR_WIDTH : natural := 13;
-    SDRAM_DATA_WIDTH : natural := 16;
-    SDRAM_COL_WIDTH : natural := 9;
-    SDRAM_ROW_WIDTH : natural := 13;
-    SDRAM_BANK_WIDTH : natural := 2;
-    CAS_LATENCY : natural := 2;
-    T_DESL : real := 200000.0;
+    SDRAM_ADDR_WIDTH : integer := 13;
+    SDRAM_DATA_WIDTH : integer := 16;
+    SDRAM_COL_WIDTH : integer := 9;
+    SDRAM_ROW_WIDTH : integer := 13;
+    SDRAM_BANK_WIDTH : integer := 2;
+    CAS_LATENCY : integer := 2;
+    T_DESL : real := 200_000.0;
     T_MRD : real := 12.0;
     T_RC : real := 60.0;
     T_RCD : real := 18.0;
     T_RP : real := 18.0;
-    T_WR : real := 12.0;
-    T_REFI : real := 7800.0
+    T_DPL : real := 14.0;
+    T_REF : real := 64_000_000.0
   );
   port (
     -- Reset signal.
@@ -81,114 +81,60 @@ end xram_sdram;
 
 architecture rtl of xram_sdram is
   -- Address bits for 32-bit words (i.e. log2(bytesize)-2)
-  constant C_ADDR_WIDTH : natural := SDRAM_COL_WIDTH+SDRAM_ROW_WIDTH+SDRAM_BANK_WIDTH-1;
+  constant C_ADDR_WIDTH : integer := SDRAM_COL_WIDTH+SDRAM_ROW_WIDTH+SDRAM_BANK_WIDTH-1;
 
-  signal s_addr : unsigned(C_ADDR_WIDTH-1 downto 0);
+  signal s_adr : std_logic_vector(C_ADDR_WIDTH-1 downto 0);
   signal s_req : std_logic;
-  signal s_ready : std_logic;
-  signal s_ack : std_logic;
-  signal s_valid : std_logic;
-
-  signal s_sdram_a : unsigned(SDRAM_ADDR_WIDTH-1 downto 0);
-  signal s_sdram_ba : unsigned(SDRAM_BANK_WIDTH-1 downto 0);
-
-  signal s_req_from_wb : std_logic;
-  signal s_start_req : std_logic;
-  signal s_wating_for_ack : std_logic;
-  signal s_waiting_for_read_response : std_logic;
-  signal s_waiting_for_write_response : std_logic;
 begin
-  -- Convert the Wishbone address to an address for the SDRAM controller.
-  s_addr <= unsigned(i_wb_adr(C_ADDR_WIDTH-1 downto 0));
-
-  -- Should & can we start a new request?
-  s_req_from_wb <= i_wb_cyc and i_wb_stb;
-  s_start_req <= s_req_from_wb and s_ready;
-
-  -- Keep the SDRAM controller REQ signal high until we've got the final ACK.
-  -- TODO(m): Correct/necessary?
-  s_req <= s_start_req or s_wating_for_ack;
-
-  -- Wishbone outputs.
-  o_wb_ack <= (s_waiting_for_read_response and s_valid) or
-              (s_waiting_for_write_response and s_ack);
-  o_wb_stall <= not s_ready;
+  -- Wishbone adaptations.
+  s_req <= i_wb_cyc and i_wb_stb;
+  s_adr <= i_wb_adr(C_ADDR_WIDTH-1 downto 0);
   o_wb_err <= '0';
 
-  -- Convert some SDRAM outputs to SLV.
-  o_sdram_a <= std_logic_vector(s_sdram_a);
-  o_sdram_ba <= std_logic_vector(s_sdram_ba);
-
-  -- Keep track of ongoing requests.
-  process (i_rst, i_wb_clk)
-  begin
-    if i_rst = '1' then
-      s_wating_for_ack <= '0';
-      s_waiting_for_read_response <= '0';
-      s_waiting_for_write_response <= '0';
-    elsif rising_edge(i_wb_clk) then
-      if s_start_req = '1' then
-        s_wating_for_ack <= '1';
-        s_waiting_for_read_response <= not i_wb_we;
-        s_waiting_for_write_response <= i_wb_we;
-      else
-        if s_ack = '1' then
-          s_wating_for_ack <= '0';
-          s_waiting_for_write_response <= '0';
-        end if;
-        if s_valid = '1' then
-          s_waiting_for_read_response <= '0';
-        end if;
-      end if;
-    end if;
-  end process;
-
   -- Instantiate the SDRAM controller.
-  sdram_controller_1: entity work.sdram
+  sdram_controller_1: entity work.sdram_controller
     generic map (
-      CLK_FREQ => real(CPU_CLK_HZ)*0.000001,
-      ADDR_WIDTH => C_ADDR_WIDTH,
-      DATA_WIDTH => 32,
-      SDRAM_ADDR_WIDTH => SDRAM_ADDR_WIDTH,
-      SDRAM_DATA_WIDTH => SDRAM_DATA_WIDTH,
-      SDRAM_COL_WIDTH => SDRAM_COL_WIDTH,
-      SDRAM_ROW_WIDTH => SDRAM_ROW_WIDTH,
-      SDRAM_BANK_WIDTH => SDRAM_BANK_WIDTH,
-      CAS_LATENCY => CAS_LATENCY,
-      BURST_LENGTH => 2,
-      T_DESL => T_DESL,
-      T_MRD => T_MRD,
-      T_RC => T_RC,
-      T_RCD => T_RCD,
-      T_RP => T_RP,
-      T_WR => T_WR,
-      T_REFI => T_REFI
+      G_CLK_FREQ_HZ => CPU_CLK_HZ,
+      G_ADDR_WIDTH => C_ADDR_WIDTH,
+      G_DATA_WIDTH => 32,
+      G_SDRAM_A_WIDTH => SDRAM_ADDR_WIDTH,
+      G_SDRAM_DQ_WIDTH => SDRAM_DATA_WIDTH,
+      G_SDRAM_BA_WIDTH => SDRAM_BANK_WIDTH,
+      G_SDRAM_COL_WIDTH => SDRAM_COL_WIDTH,
+      G_SDRAM_ROW_WIDTH => SDRAM_ROW_WIDTH,
+      G_CAS_LATENCY => CAS_LATENCY,
+      G_T_DESL => T_DESL,
+      G_T_MRD => T_MRD,
+      G_T_RC => T_RC,
+      G_T_RCD => T_RCD,
+      G_T_RP => T_RP,
+      G_T_DPL => T_DPL,
+      G_T_REF => T_REF
     )
     port map (
-      reset  => i_rst,
-      clk => i_wb_clk,
+      i_rst  => i_rst,
+      i_clk => i_wb_clk,
 
       -- CPU/Wishbone interface.
-      addr => s_addr,
-      data => i_wb_dat,
-      we => i_wb_we,
-      sel => i_wb_sel,
-      req => s_req,
-      ready => s_ready,
-      ack => s_ack,
-      valid => s_valid,
-      q => o_wb_dat,
+      i_adr => s_adr,
+      i_dat_w => i_wb_dat,
+      i_we => i_wb_we,
+      i_sel => i_wb_sel,
+      i_req => s_req,
+      o_busy => o_wb_stall,
+      o_ack => o_wb_ack,
+      o_dat => o_wb_dat,
 
       -- External SDRAM interface.
-      sdram_a => s_sdram_a,
-      sdram_ba => s_sdram_ba,
-      sdram_dq => io_sdram_dq,
-      sdram_cke => o_sdram_cke,
-      sdram_cs_n => o_sdram_cs_n,
-      sdram_ras_n => o_sdram_ras_n,
-      sdram_cas_n => o_sdram_cas_n,
-      sdram_we_n => o_sdram_we_n,
-      sdram_dqm => o_sdram_dqm
+      o_sdram_a => o_sdram_a,
+      o_sdram_ba => o_sdram_ba,
+      io_sdram_dq => io_sdram_dq,
+      o_sdram_cke => o_sdram_cke,
+      o_sdram_cs_n => o_sdram_cs_n,
+      o_sdram_ras_n => o_sdram_ras_n,
+      o_sdram_cas_n => o_sdram_cas_n,
+      o_sdram_we_n => o_sdram_we_n,
+      o_sdram_dqm => o_sdram_dqm
     );
 
 end architecture rtl;
